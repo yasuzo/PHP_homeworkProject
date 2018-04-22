e5cbf9cc3746e6861810a4ea9d647c011733eaae
jan.capek <jan.capek@fer.hr>
2018-04-22 11:49:11 +0200

    test_funkcije

---
+++ src/dz/test_funkcije.php
@@ -0,0 +1,38 @@
+<?php
+require_once 'funkcije.php';
+
+// ponavljanje()
+
+echo ponavljanje("abcdefhij", "c", "a", "h").'<br>';
+echo ponavljanje("žđščćžđš", "ć", "ž", "š").'<br>';
+echo ponavljanje("abcdef", "a ", "b").'<br>';
+echo ponavljanje("abcdef", "a", "b", "c ").'<br>';
+echo ponavljanje("!@#$%", "$", "!", "@").'<br>';
+echo ponavljanje("abcdefghij", "l", "a", "b").'<br>';
+
+// zbroji()
+
+echo '<br>';
+echo zbroji("12345").'<br>';
+echo zbroji("12e345").'<br>';
+echo zbroji("12.345").'<br>';
+echo zbroji("12345 ").'<br>';
+echo zbroji("-12").'<br>';
+echo zbroji("00000").'<br>';
+echo zbroji("9999999").'<br>';
+echo zbroji("123456789123456789123456789").'<br>';
+
+// transformiraj()
+
+var_dump(transformiraj("#hi#"));
+var_dump(transformiraj("<b>#hi#</b>"));
+var_dump(transformiraj("*hi*"));
+var_dump(transformiraj("'hi'"));
+var_dump(transformiraj("#hi"));
+var_dump(transformiraj("*hi#hi*hi#"));
+var_dump(transformiraj("#hi#hi#hi#"));
+var_dump(transformiraj("#hi#hi*hi#"));
+var_dump(transformiraj('"#$#"'));
+var_dump(transformiraj("*hi#hi#hi*"));
+var_dump(transformiraj("*#'a'#*"));
+var_dump(transformiraj("đ*s#ć'čć'đ#ž*č"));
