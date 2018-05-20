5cb57f40c4a83a33b6fc009b21c499f289bf1d31
yasuzo <jan.capek@fer.hr>
2018-05-20 19:25:42 +0200

    Text normalizing functions added

--- src/dz/zamjena.php
+++ src/dz/zamjena.php
@@ -19,14 +19,11 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
                 send_message("Greska - nije moguće pročitati sadržaj datoteke!");
             else if(($data = transformiraj($data)) === null)
                 send_message("Greska - svi tagovi u datoteci moraju biti zatvoreni i moraju se zatvarati suprotno od onoga kako su se otvarali!");
-            else if(file_put_contents('transformirani.html', $data)=== false)
-                send_message("Greska - nije moguce stvoriti novu datoteku!");
             else{
                 $show = false;
                 header("Content-Type: application/html");
                 header("Content-Disposition: attachment; filename='transformirani.html'");
-                readfile("transformirani.html");
-                unlink('transformirani.html');
+                echo $data;
             }
         }
     }
--- src/practice/Predavanja/zad3.php
+++ src/practice/Predavanja/zad3.php
@@ -2,17 +2,36 @@
 
 declare(strict_types=1);
 
-class Formater{
+interface Formater{
+    public function formatiraj(string $ulaz): string;
+}
+
+class CompositeFormater implements Formater{
+    private $formateri;
+
+    public function __construct(array $format){
+        foreach($format as $el){
+            $formateri[] = $el;
+        }
+    }
+
+    public function formatiraj(string $ulaz): string {
+        foreach($foramteri as $formater){
+            $ulaz = $formater->formatiraj($ulaz);
+        }
+
+        return $ulaz;
+    }
+}
+
+class StrongFormater implements Formater{
     public function formatiraj(string $ulaz): string{
         return '<strong>'.$ulaz.'</strong>';
     }
 }
 
-class NewFormater extends Formater{
-
-
+class ItalicsFormater implements Formater{
     public function formatiraj(string $ulaz): string{
-        $ulaz = parent::formatiraj($ulaz);
         return '<i>'.$ulaz.'</i>';
     }
 }
@@ -20,7 +39,7 @@ class NewFormater extends Formater{
 class Editor{
     private $formater;
     public function __construct(Formater $formater){
-        $this->formater = $formater; 
+        $this->formater = $formater;
     }
 
     public function uredi(string ...$redovi): string{
@@ -33,7 +52,7 @@ class Editor{
 
 $object1 = new Formater();
 
-$object2 = new NewFormater();
+$object2 = new ItalicsFormater();
 
 $editor = new Editor($object1);
 echo $editor->uredi('ovo je bold!', 'blabla');
